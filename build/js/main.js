a09d6fa1dec79df169be7a27f65ee312b8adb3ee
fabiochiqufai <fabiochiquezi@gmail.com>
2018-12-11 02:34:46 -0200

    Arquivos de Git-Gulp

---
+++ build/js/main.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// alert('aaa');
+console.log('cccc');
---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+
+//Sass
+function compilaSass() {
+  return gulp
+  .src('src/scss/*.scss')
+  .pipe(sass())
+  .pipe(autoprefixer({
+    browsers: ['last 5 versions'],
+    cascade: false
+  }))
+  .pipe(gulp.dest('build/css/'))
+  .pipe(browserSync.stream());
+}
+
+gulp.task('sass', compilaSass);
+
+// BrowserSync
+function browser() {
+  browserSync.init({
+      server: {
+          baseDir: "./build"
+      }
+  });
+};
+
+gulp.task('browser-sync', browser)
+
+//Concat
+function gulpJs(){
+  return gulp
+    .src('./src/js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(gulp.dest('./build/js'));
+}
+
+gulp.task('gulp-concat', gulpJs);
+
+//Watch
+function watch() {
+  gulp.watch('src/scss/*.scss', compilaSass);
+  gulp.watch('src/js/*.js', gulpJs);
+  gulp.watch(['build/*.html', 'build/*.php']).on('change', browserSync.reload);
+}
+
+gulp.task('watch', watch)
+
+//Default
+gulp.task('default', gulp.parallel('watch', 'browser-sync', 'sass', 'gulp-concat'));
